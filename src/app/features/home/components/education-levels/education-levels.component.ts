172ceee01509aee4adaef31031739cc38a25e28f
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2023-12-28 15:44:32 -0300

    Refactor: Home - Mouseover and mouseout events

--- src/app/features/home/components/education-levels/education-levels.component.ts
+++ src/app/features/home/components/education-levels/education-levels.component.ts
@@ -3,18 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-education-levels',
   templateUrl: './education-levels.component.html',
-  styleUrl: './education-levels.component.css'
+  styleUrl: './education-levels.component.css',
 })
 export class EducationLevelsComponent {
-  babyPhotoDefault:string = "assets/images/babyClass3.jpg";
-  babyPhoto:string = this.babyPhotoDefault;
+  babyPhoto: string = 'assets/images/babyClass3.jpg';
+  kidPhoto: string = 'assets/images/kidClass2.jpg';
+  teenPhoto: string = 'assets/images/teenClass1.jpg';
+  youngAdultPhoto: string = 'assets/images/youngAdultClass3.jpg';
 
-  kidPhotoDefault:string = "assets/images/kidClass2.jpg";
-  kidPhoto:string = this.kidPhotoDefault;
+  onMouserOver(lifeStage: string): void {
+    if (lifeStage === 'baby') this.babyPhoto = 'assets/images/babyClass8.jpg';
+    if (lifeStage === 'kid') this.kidPhoto = 'assets/images/kidClass1.jpg';
+    if (lifeStage === 'teen') this.teenPhoto = 'assets/images/teenClass4.jpg';
+    if (lifeStage === 'youngAdult')
+      this.youngAdultPhoto = 'assets/images/youngAdultClass4.jpg';
+  }
 
-  teenPhotoDefault:string = "assets/images/teenClass1.jpg";
-  teenPhoto:string = this.teenPhotoDefault;
-
-  youngAdultPhotoDefault:string = "assets/images/youngAdultClass3.jpg";
-  youngAdultPhoto:string = this.youngAdultPhotoDefault;
+  onMouserOut(lifeStage: string): void {
+    if (lifeStage === 'baby') this.babyPhoto = 'assets/images/babyClass3.jpg';
+    if (lifeStage === 'kid') this.kidPhoto = 'assets/images/kidClass2.jpg';
+    if (lifeStage === 'teen') this.teenPhoto = 'assets/images/teenClass1.jpg';
+    if (lifeStage === 'youngAdult')
+      this.youngAdultPhoto = 'assets/images/youngAdultClass3.jpg';
+  }
 }
