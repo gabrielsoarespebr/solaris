963413ab9b27a3312e082af76ce305c6f61b3fa5
Jorge Freitas <jlcf04@gmail.com>
2023-12-29 01:07:55 -0300

    add image transitions

--- src/app/features/home/components/education-levels/education-levels.component.ts
+++ src/app/features/home/components/education-levels/education-levels.component.ts
@@ -1,29 +1,74 @@
-import { Component } from '@angular/core';
+// education-levels.component.ts
+
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-education-levels',
-  templateUrl: './education-levels.component.html',
-  styleUrl: './education-levels.component.css',
+  selector: "app-education-levels",
+  templateUrl: "./education-levels.component.html",
+  styleUrls: ["./education-levels.component.css"],
 })
 export class EducationLevelsComponent {
-  babyPhoto: string = 'assets/images/babyClass3.jpg';
-  kidPhoto: string = 'assets/images/kidClass2.jpg';
-  teenPhoto: string = 'assets/images/teenClass1.jpg';
-  youngAdultPhoto: string = 'assets/images/youngAdultClass3.jpg';
+  babyPhoto: string = "assets/images/babyClass3.jpg";
+  kidPhoto: string = "assets/images/kidClass2.jpg";
+  teenPhoto: string = "assets/images/teenClass1.jpg";
+  youngAdultPhoto: string = "assets/images/youngAdultClass3.jpg";
+
+  // Utiliza um objeto para rastrear a classe de transição de cada item
+  fadeClasses: { [key: string]: string } = {
+    baby: "",
+    kid: "",
+    teen: "",
+    youngAdult: "",
+  };
 
   onMouserOver(lifeStage: string): void {
-    if (lifeStage === 'baby') this.babyPhoto = 'assets/images/babyClass8.jpg';
-    if (lifeStage === 'kid') this.kidPhoto = 'assets/images/kidClass1.jpg';
-    if (lifeStage === 'teen') this.teenPhoto = 'assets/images/teenClass4.jpg';
-    if (lifeStage === 'youngAdult')
-      this.youngAdultPhoto = 'assets/images/youngAdultClass4.jpg';
+    this.fadeClasses[lifeStage] = "fade-in";
+    setTimeout(() => {
+      this.updateImage(lifeStage);
+      this.fadeClasses[lifeStage] = "fade-out";
+    }, 300);
   }
 
   onMouserOut(lifeStage: string): void {
-    if (lifeStage === 'baby') this.babyPhoto = 'assets/images/babyClass3.jpg';
-    if (lifeStage === 'kid') this.kidPhoto = 'assets/images/kidClass2.jpg';
-    if (lifeStage === 'teen') this.teenPhoto = 'assets/images/teenClass1.jpg';
-    if (lifeStage === 'youngAdult')
-      this.youngAdultPhoto = 'assets/images/youngAdultClass3.jpg';
+    this.fadeClasses[lifeStage] = "fade-in";
+    setTimeout(() => {
+      this.resetImages(lifeStage);
+      this.fadeClasses[lifeStage] = "fade-out";
+    }, 300);
+  }
+
+  private updateImage(lifeStage: string): void {
+    // Atualiza a imagem com base na fase de vida
+    switch (lifeStage) {
+      case "baby":
+        this.babyPhoto = "assets/images/babyClass8.jpg";
+        break;
+      case "kid":
+        this.kidPhoto = "assets/images/kidClass1.jpg";
+        break;
+      case "teen":
+        this.teenPhoto = "assets/images/teenClass4.jpg";
+        break;
+      case "youngAdult":
+        this.youngAdultPhoto = "assets/images/youngAdultClass4.jpg";
+        break;
+    }
+  }
+
+  private resetImages(lifeStage: string): void {
+    switch (lifeStage) {
+      case "baby":
+        this.babyPhoto = "assets/images/babyClass3.jpg";
+        break;
+      case "kid":
+        this.kidPhoto = "assets/images/kidClass2.jpg";
+        break;
+      case "teen":
+        this.teenPhoto = "assets/images/teenClass1.jpg";
+        break;
+      case "youngAdult":
+        this.youngAdultPhoto = "assets/images/youngAdultClass3.jpg";
+        break;
+    }
   }
 }
