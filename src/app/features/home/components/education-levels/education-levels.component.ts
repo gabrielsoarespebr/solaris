d116f5bd6d1d6b52c17ca7441f0bd38b8fba7ec5
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-01-04 23:12:06 -0300

    fix: Education levels - Function names

--- src/app/features/home/components/education-levels/education-levels.component.ts
+++ src/app/features/home/components/education-levels/education-levels.component.ts
@@ -11,7 +11,7 @@ export class EducationLevelsComponent {
   teenPhoto: string = 'assets/images/teenClass1.jpg';
   youngAdultPhoto: string = 'assets/images/youngAdultClass3.jpg';
 
-  onMouserOver(lifeStage: string): void {
+  onMouseOver(lifeStage: string): void {
     if (lifeStage === 'baby') this.babyPhoto = 'assets/images/babyClass8.jpg';
     if (lifeStage === 'kid') this.kidPhoto = 'assets/images/kidClass1.jpg';
     if (lifeStage === 'teen') this.teenPhoto = 'assets/images/teenClass4.jpg';
@@ -19,7 +19,7 @@ export class EducationLevelsComponent {
       this.youngAdultPhoto = 'assets/images/youngAdultClass4.jpg';
   }
 
-  onMouserOut(lifeStage: string): void {
+  onMouseOut(lifeStage: string): void {
     if (lifeStage === 'baby') this.babyPhoto = 'assets/images/babyClass3.jpg';
     if (lifeStage === 'kid') this.kidPhoto = 'assets/images/kidClass2.jpg';
     if (lifeStage === 'teen') this.teenPhoto = 'assets/images/teenClass1.jpg';
